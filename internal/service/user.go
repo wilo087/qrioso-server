0dac4c474ca13ff5af2bff9eef4c4143e9909da8
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-07-06 18:32:58 -0400

    Adding handler, repository and service for user

--- internal/service/user.go
+++ internal/service/user.go
@@ -1,7 +1,23 @@
 package service
 
-import "github.com/wilo0087/qrioso-server/internal/repository"
+import (
+	"github.com/wilo0087/qrioso-server/internal/model"
+	"github.com/wilo0087/qrioso-server/internal/repository"
+)
 
 type UserService struct {
 	userRepository *repository.UserRepository
+}
+
+func NewUserService(ur *repository.UserRepository) *UserService {
+	return &UserService{userRepository: ur}
+}
+
+func (u *UserService) GetUserByID(id string) (*model.User, error) {
+	user, err := u.userRepository.GetUserByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
