9baf257731f6683843852e1ba8dd043e38ceced4
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-07-06 18:19:29 -0400

    Fixing models

--- internal/model/company.go
+++ internal/model/company.go
@@ -2,17 +2,53 @@ package model
 
 import (
 	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Company struct {
-	ID               uint64           `gorm:"primaryKey;auto_increment"`
-	Name             string           `gorm:"not null"`
-	Address          string           `gorm:"default:null"`
-	Phone            string           `gorm:"not null"`
-	Email            string           `gorm:"not null"`
-	Website          string           `gorm:"default:null"`
-	Users            []User           `gorm:"many2many:users_on_companies"`
-	UsersOnCompanies []UsersOnCompany `gorm:"foreignKey:CompanyID"`
-	CreatedAt        time.Time        `gorm:"default:current_timestamp;column:created_at"`
-	UpdatedAt        time.Time        `gorm:"autoUpdateTime;column:updated_at"`
+	BaseUUID
+	Name    string `gorm:"size:255"`
+	Address int
+	Website string `gorm:"size:255"`
+	Status  CompanyStatus
+	Users   []User          `gorm:"many2many:user_on_companies"`
+	Configs []CompanyConfig `gorm:"foreignKey:CompanyID"`
+	BaseModelSoftDelete
 }
+
+type UserOnCompany struct {
+	CompanyID uuid.UUID `gorm:"primaryKey"`
+	UserID    uuid.UUID `gorm:"primaryKey"`
+	Status    UserOnCompanyStatus
+	IsAdmin   bool
+	BaseModelSoftDelete
+}
+
+type CompanyConfig struct {
+	UUID       uint      `gorm:"primaryKey"`
+	CompanyID  uuid.UUID `gorm:"type:uuid"`
+	Config     string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeletedAt  gorm.DeletedAt `gorm:"index"`
+	ConfigType string
+}
+
+type CompanyStatus string
+
+const (
+	CompanyStatusVerified    CompanyStatus = "verified"
+	CompanyStatusActive      CompanyStatus = "active"
+	CompanyStatusUnpublished CompanyStatus = "unpublished"
+	CompanyStatusDeleted     CompanyStatus = "deleted"
+)
+
+type UserOnCompanyStatus string
+
+const (
+	UserOnCompanyStatusDisable UserOnCompanyStatus = "disable"
+	UserOnCompanyStatusActive  UserOnCompanyStatus = "active"
+	UserOnCompanyStatusDeleted UserOnCompanyStatus = "deleted"
+)
